00e020dc217767678521c470fdce2c53e3ed017a
Marcos <maasouza@poli.ufrj.br>
2016-02-05 16:16:30 -0200

    teste jogo
    app

---
+++ Main/js/main.js
@@ -0,0 +1,21 @@
+//Carregando a dependencia three-world
+var Mundo = require('three-world')
+
+//função de update de frame
+function render() {
+}
+
+//Iniciando o mundo
+Mundo.init({ renderCallback: render })
+
+var tunnel = new THREE.Mesh(
+  new THREE.CylinderGeometry(100, 100, 5000, 24, 24, true),
+  new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('images/0.jpg'),
+    side: THREE.BackSide
+  })
+)
+tunnel.rotation.x = -Math.PI/2
+World.add(tunnel)
+
+Mundo.start()
