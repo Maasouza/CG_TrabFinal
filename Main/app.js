836a00d9718564da159c4216474f7d33585ce782
Marcos <maasouza@poli.ufrj.br>
2016-02-06 21:41:51 -0200

    loaders do modelo e do material

--- Main/app.js
+++ Main/app.js
@@ -16,16 +16,17 @@ var wormhole = new THREE.Mesh(
   new THREE.CylinderGeometry(100, 100, 5000, 24, 24, true),
   new THREE.MeshBasicMaterial({
     //utilizando a textura na parte de dentro do cilindro
-    map: THREE.ImageUtils.loadTexture('images/1.jpg',null,function(textura){
-      textura.wrapS = textura.wrapT = THREE.RepeatWrapping
-      textura.repeat.set(5,10)
-      tex.needsUpdate = true //textura é atualizada dependendo da posição da nava
-    }),
+    map: THREE.ImageUtils.loadTexture('images/1.jpg')//,null,function(textura){
+      //textura.wrapS = textura.wrapT = THREE.RepeatWrapping
+      //textura.repeat.set(5,10)
+    //  tex.needsUpdate = true //textura é atualizada dependendo da posição da nava
+    //})
+    ,
     side: THREE.BackSide
   })
 )
 //Adicionando nuvem a cena
-Mundo.getScene().fog = THREE.FogExp2(0x000022,0.00125)
+Mundo.getScene().fog = THREE.FogExp2(0x000022,0.0125)
 //rotacionando o cilindro para pos frontal a camera
 wormhole.rotation.x = -Math.PI/2
 
