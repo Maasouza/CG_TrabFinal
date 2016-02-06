31c8732e606f29872296366745006466397ef1c5
Marcos <maasouza@poli.ufrj.br>
2016-02-06 15:28:13 -0200

    edit menu , fog
    nao redireciona mais para o jogo diretamente

--- Main/js/main.js
+++ Main/js/main.js
@@ -15,7 +15,11 @@ var wormhole = new THREE.Mesh(
   new THREE.CylinderGeometry(100, 100, 5000, 24, 24, true),
   new THREE.MeshBasicMaterial({
     //utilizando a textura na parte de dentro do cilindro
-    map: THREE.ImageUtils.loadTexture('images/1.jpg'),
+    map: THREE.ImageUtils.loadTexture('images/1.jpg',null,function(textura){
+      textura.wrapS = textura.wrapT = THREE.RepeatWrapping
+      textura.repeat.set(5,10)
+      tex.needsUpdate = true //textura é atualizada dependendo da posição da nava
+    }),
     side: THREE.BackSide
   })
 )
