1b3193f2b8c58df9aeea6d3e6b8eccdbc0a27c24
Marcos <maasouza@poli.ufrj.br>
2016-02-06 22:10:55 -0200

    upload models
    repair images

--- Main/js/main.js
+++ Main/js/main.js
@@ -1,7 +1,7 @@
 //Carregando a dependencia three-world
+var myLoader = require('./objmtlloader')
 var Mundo = require('three-world')
 var THREE = require('three')
-
 //função de update de frame
 function render() {
 }
@@ -15,19 +15,33 @@ var wormhole = new THREE.Mesh(
   new THREE.CylinderGeometry(100, 100, 5000, 24, 24, true),
   new THREE.MeshBasicMaterial({
     //utilizando a textura na parte de dentro do cilindro
-    map: THREE.ImageUtils.loadTexture('images/1.jpg')//,null,function(textura){
-      //textura.wrapS = textura.wrapT = THREE.RepeatWrapping
-      //textura.repeat.set(5,10)
-    //  tex.needsUpdate = true //textura é atualizada dependendo da posição da nava
-    //})
-    ,
+    map: THREE.ImageUtils.loadTexture('images/1.jpg',null,function(textura){
+      textura.wrapS = textura.wrapT = THREE.RepeatWrapping
+      textura.repeat.set(5,10)
+      tex.needsUpdate = true //textura é atualizada dependendo da posição da nava
+    }),
     side: THREE.BackSide
   })
 )
+
 //Adicionando nuvem a cena
 Mundo.getScene().fog = THREE.FogExp2(0x000022,0.0125)
+
 //rotacionando o cilindro para pos frontal a camera
 wormhole.rotation.x = -Math.PI/2
+
+var objLoad = new THREE.OBJMTLLoader();
+
+objLoad.load(
+  //objeto
+  '../obj/craft.obj',
+  //material
+  '../obj/craft.obj',
+  //quando carrega-los
+  function(object){
+    Mundo.add(object)
+  }
+)
 
 
 Mundo.add(wormhole)
